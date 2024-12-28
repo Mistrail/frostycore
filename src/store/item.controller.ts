886083fcffd9a54852059ac6b529cb6a85b9ae59
Mistrail <frigus.ventus@gmail.com>
2024-12-28 12:59:49 +0200

    Minor fixes

--- src/store/item.controller.ts
+++ src/store/item.controller.ts
@@ -15,43 +15,36 @@ export class ItemController {
 
     constructor(private readonly service: StorageService) { }
 
-    @ApiOperation({ summary: 'Создать  товар на складе' })
     @Post('new')
     createOne(@Body() fields: ItemCreateDto): Promise<Item> {
         return this.service.createItem(fields)
     }
 
-    @ApiOperation({ summary: 'Поиск товаров на складе' })
     @Get('search')
     getList(s: string): Promise<List<Item>> {
         return this.service.searchItems(s)
     }
 
-    @ApiOperation({ summary: 'Список товаров по ууказанному фильтру' })
     @Get('list')
     search(@Query() filter?: FilterDto[]): Promise<List<Item>> {
         return this.service.getItems(filter)
     }
 
-    @ApiOperation({ summary: 'Получить товар по ИД' })
     @Get(':id')
     getOne(@Param('id', ParseIntPipe) id: number): Promise<Item> {
         return this.service.getItem(id)
     }
 
-    @ApiOperation({ summary: 'Обновить информацию о товаре' })
     @Put(':id')
     updateOne(@Param('id', ParseIntPipe) id: number, @Body() fields: ItemUpdateDto): Promise<Item> {
         return this.service.updateItem(id, fields)
     }
 
-    @ApiOperation({ summary: 'Обновить свойства товара' })
     @Patch(':id')
     updateProps(@Param('id', ParseIntPipe) id: number, @Body() props: PropertyValueUpdateDto[]): Promise<Item> {
         return this.service.upateItemProps(id, props)
     }
 
-    @ApiOperation({ summary: 'Удалить товар' })
     @Delete(':id')
     removeOne(@Param('id', ParseIntPipe) id: number): true {
         return this.service.removeItem(id)
--- src/store/storage.controller.ts
+++ src/store/storage.controller.ts
@@ -14,37 +14,31 @@ export class StorageController {
 
     constructor(private readonly service: StorageService) { }
 
-    @ApiOperation({ summary: 'Создать новый склад' })
     @Post('new')
     createOne(@Body() fields: StoreCreateDto): Promise<Store>  {
         return this.service.createStore(fields)
     }
 
-    @ApiOperation({ summary: 'Получить список складов по фильтру' })
     @Get('list')
     getList(@Query() filter?: FilterDto[]): Promise<List<Store>> {
         return this.service.getStores(filter)
     }
 
-    @ApiOperation({ summary: 'Поиск складов' })
     @Get('search')
     search(s: string): Promise<List<Store>> {
         return this.service.searchStores(s)
     }
 
-    @ApiOperation({ summary: 'Получить склад по ИД' })
     @Get(':id')
     getOne(@Param('id', ParseIntPipe) id: number): Promise<Store>  {
         return this.service.getStore(id)
     }
 
-    @ApiOperation({ summary: 'Обновить информацию о складе' })
     @Put(':id')
     updateOne(@Param('id', ParseIntPipe) id: number, @Body() fields: StoreUpdateDto) {
         return this.service.updateStore(id, fields)
     }
 
-    @ApiOperation({ summary: 'Удалить склад' })
     @Delete(':id')
     removeOne(@Param('id', ParseIntPipe) id: number): true {
         return this.service.removeStore(id)
